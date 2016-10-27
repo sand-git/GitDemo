ad393a75626030c972aca7557e69c6dd5dd10b5f
bluesky <sand@bluedeMacBook-Pro.local>
2016-10-27 14:02:01 +0800

    上传新的项目
    提交新的项目代码

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by blue sky on 2016/10/27.
+//  Copyright © 2016年 sand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by blue sky on 2016/10/27.
+//  Copyright © 2016年 sand. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
